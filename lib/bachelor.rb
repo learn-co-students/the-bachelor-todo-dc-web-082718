d7c04a98084a71ee36edeca04b51d064bb9b57be
kruchkamgar <kruchkamgar@gmail.com>
2018-07-11 21:47:29 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  h_contestant = data[season].select { |hash|
+    hash.values.include? "Winner"
+  }
+
+  h_contestant[0]["name"].split(' ').first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
+  data.each { |season, a_ctst|
+    # binding.pry
+    h_contestant = a_ctst.keep_if { |hash|
+      hash.values.include? occupation
+    }
+    if !h_contestant.empty? then return h_contestant[0]["name"] end
+  }
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+count = 0
+
+  data.each { |season, a_ctst|
+  count += a_ctst.collect { |hash|
+      hash.values.include? hometown
+    }.count(true)
+  }
+
+count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each { |season, a_ctst|
+    a_ctst.each { |hash|
+      if hash.values.include? hometown then return hash["occupation"] end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = data[season].collect { |hash|
+    hash["age"].to_i
+  }
+
+(ages.inject{ |sum, age| sum + age }/ages.length.to_f).round
 end
