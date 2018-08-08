ac3578b24be62261c423e112d835ceadddcc7653
angelica winkler <caw400@nyu.edu>
2018-08-08 14:53:20 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,114 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  contestantStatuses = []
+
+  season = data.fetch(season)
+  season.each do |personalHash, pairs|
+    personalHash.each do |key, value|
+      if key == "status"
+        contestantStatuses << value
+
+      end
+    end
+  end
+  winnerIndex = contestantStatuses.index("Winner")
+
+  winnerName = []
+
+  winnerHash = season[winnerIndex]
+  winnerHash.each do |question, answer|
+    if question == "name"
+      winnerName << answer
+    end
+  end
+
+  winnerName = winnerName.join(" ").split(" ")
+  winnerName = winnerName[0]
+  winnerName
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestantHash = {}
+
+  data.each do |season, contestants|
+    contestants.each do |personalHash, pairs|
+      personalHash.each do |question, answer|
+        if question == "occupation"
+          if answer == occupation
+            contestantHash = personalHash
+          end
+        end
+      end
+    end
+  end
+  contestantHash["name"]
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  fromHomeTown = []
+
+  data.each do |season, contestants|
+    contestants.each do |personalHash, pairs|
+      personalHash.each do |question, answer|
+        if question == "hometown"
+          if answer == hometown
+            fromHomeTown << hometown
+          end
+        end
+      end
+    end
+  end
+  fromHomeTown.length
 end
 
+
+
 def get_occupation(data, hometown)
-  # code here
+  fromHomeTown = []
+
+  data.each do |season, contestants|
+    contestants.each do |personalHash, pairs|
+      personalHash.each do |question, answer|
+        if question == "hometown"
+          if answer == hometown
+            fromHomeTown << personalHash["occupation"]
+          end
+        end
+      end
+    end
+  end
+  fromHomeTown[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+
+  season = data.fetch(season)
+  season.each do |personalHash, pairs|
+    personalHash.each do |key, value|
+      if key == "age"
+        ages << value
+      end
+    end
+  end
+  ages
+  numAges = []
+
+  i = 0
+  while i < ages.length
+    numAges << ages[i].to_f
+    i += 1
+  end
+  sum = 0
+  i = 0
+
+  while i < numAges.length
+    sum += numAges[i]
+    i += 1
+  end
+  sum = sum / numAges.length
+  sum.round
 end
